7fb60dc87638bf5d246109c8fa494cc8ceb99273
PhilLar <phillarov@gmail.com>
2019-09-03 15:39:17 +0300

    refined short package from unnecessary functionality

--- short/short.go
+++ short/short.go
@@ -4,50 +4,17 @@ import (
 	"bytes"
 	"encoding/json"
 	"errors"
-	"flag"
-	//"fmt"
 	"io/ioutil"
-	//"log"
 	"net/http"
 	"net/url"
-	"sync"
 )
 
 const cleanURLAPI string = "https://cleanuri.com/api/v1/shorten"
 const relinkAPI string = "https://rel.ink/api/links/"
 
-var flagRelink *bool = flag.Bool("relink", false, "use rel.ink service to shorten URL")
-
 type cleanURLAnswer struct {
 	ResultURL string `json:"result_url"`
 	Error     string
-}
-
-//RunInParallel is a wrapper for cleanURL() and Relink()
-func RunInParallel(a func(urlLink string) (string, error), args []string) map[string]error {
-	shorts := make(map[string]error)
-	var mutex = &sync.Mutex{}
-	var wg sync.WaitGroup
-	wg.Add(len(args))
-	for _, arg := range args {
-		go func(arg string) {
-			defer wg.Done()
-			short, err := a(arg)
-			mutex.Lock()
-			shorts[short] = err
-			mutex.Unlock()
-		}(arg)
-	}
-	wg.Wait()
-	return shorts
-}
-
-func ShortenUrls() map[string]error {
-	flag.Parse()
-	if !*flagRelink {
-		return RunInParallel(CleanURL, flag.Args())
-	}
-	return RunInParallel(Relink, flag.Args())
 }
 
 func CleanURL(urlLink string) (string, error) {
